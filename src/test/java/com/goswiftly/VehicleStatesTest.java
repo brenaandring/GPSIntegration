8c23890014ce13b21149e905a2dba79e542e0ff5
Brena Andring <bzandring@gmail.com>
2020-09-27 13:10:54 -0700

    updated test

---
+++ src/test/java/com/goswiftly/VehicleStatesTest.java
@@ -0,0 +1,25 @@
+package com.goswiftly;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleStatesTest {
+
+    @Test
+    void processUpdatesVehicleState() {
+        VehicleStates vehicleStates = new VehicleStates();
+        GPSPing gpsPing = new GPSPing("my-tesla", 100, 10.0, 20.0);
+
+        vehicleStates.process(gpsPing);
+
+        List<Vehicle> latestVehicleStates = vehicleStates.getLatestVehicleStates();
+        List<Vehicle> expectedState = List.of(new Vehicle("my-tesla", 10.0, 20.0, 100));
+
+        assertEquals(expectedState, latestVehicleStates);
+
+
+    }
+}
