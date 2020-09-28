6afaba4776ac532308e5eccd121faceb05f1296c
Brena Andring <bzandring@gmail.com>
2020-09-27 17:14:57 -0700

    added two unit tests

--- src/test/java/com/goswiftly/VehicleStatesTest.java
+++ src/test/java/com/goswiftly/VehicleStatesTest.java
@@ -20,4 +20,39 @@ class VehicleStatesTest {
 
         assertEquals(expectedState, latestVehicleStates);
     }
+
+    @Test
+    void processDiscardsOldGPSPing() {
+        VehicleStates vehicleStates = new VehicleStates();
+        GPSPing gpsPingNew = new GPSPing("mazda", 1701220, 10.0, 20.0);
+        GPSPing gpsPingOld = new GPSPing("mazda", 1701100, 5.0, 10.0);
+
+        vehicleStates.process(gpsPingNew);
+        vehicleStates.process(gpsPingOld);
+
+        List<Vehicle> latestVehicleStates = vehicleStates.getLatestVehicleStates();
+        List<Vehicle> expectedState = List.of(new Vehicle("mazda", 10.0, 20.0, 1701220));
+
+        assertEquals(expectedState, latestVehicleStates);
+    }
+
+    @Test
+    void processMultipleGPSPings() {
+        VehicleStates vehicleStates = new VehicleStates();
+        GPSPing gpsPing1 = new GPSPing("bus1", 1205967, 15.0, 20.0);
+        GPSPing gpsPing2 = new GPSPing("bus2", 1302954, 5.0, 20.0);
+
+        vehicleStates.process(gpsPing1);
+        vehicleStates.process(gpsPing2);
+
+        List<Vehicle> latestVehicleStates = vehicleStates.getLatestVehicleStates();
+        List<Vehicle> expectedState = List.of(
+                new Vehicle("bus1", 15.0, 20.0, 1205967),
+                new Vehicle("bus2", 5.0, 20.0, 1302954)
+        );
+
+        assertEquals(expectedState, latestVehicleStates);
+
+    }
+
 }
